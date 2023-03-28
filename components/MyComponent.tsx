5e88afde12832d2d6f9600eeb3a6a39523740110
faouzi96 <faouziboussad@gmail.com>
2023-03-29 00:13:56 +0200

    add the first page Login page

--- components/MyComponent.tsx
+++ components/MyComponent.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View } from "react-native";
-import { makeStyles, Text, Button, useThemeMode } from "@rneui/themed";
+import { makeStyles, Text, useThemeMode, Input, Button } from "@rneui/themed";
+import { Feather, Entypo } from "@expo/vector-icons";
 
 export default function App() {
   const styles = useStyles();
@@ -10,13 +11,27 @@ export default function App() {
     setMode(mode === "dark" ? "light" : "dark");
   };
 
+  const loginPress = ()=>{
+    alert("button pressed")
+  }
+
   return (
     <View style={styles.container}>
-      <Text h3>Start Using RNE </Text>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
-      <Button onPress={handleOnPress}>Switch Theme</Button>
+      <View style={styles.header}>
+        <View style={styles.title}>
+          <Text h4>SMPark</Text>
+        </View>
+        <View style={styles.themeSwitcher}>
+          <Feather name="sun" size={24} color="black" />
+        </View>
+      </View>
+      <View style={styles.mainContent}>
+        <Input placeholder="USER NAME" />
+        <Input placeholder="PASSWORD" />
+        <Button radius={"sm"} type="solid" size="lg" onPress={loginPress}>
+          Login
+        </Button>
+      </View>
     </View>
   );
 }
@@ -24,9 +39,39 @@ export default function App() {
 const useStyles = makeStyles((theme) => ({
   container: {
     flex: 1,
-    backgroundColor: theme.colors.background,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  header: {
+    flex: 1,
+    flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#f00",
+    width: "100%",
+  },
+  title: {
+    flex: 1,
+    height: "100%",
+    alignItems: "flex-start",
+    justifyContent: "center",
+    marginTop: 5,
+    paddingLeft: 20,
+  },
+  themeSwitcher: {
+    flex: 1,
+    alignItems: "flex-end",
+    paddingRight: 20,
+    justifyContent: "center",
+    height: "100%",
+    paddingTop: 10,
+  },
+  mainContent: {
+    flex: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "80%",
   },
   text: {
     marginVertical: theme.spacing.lg,
